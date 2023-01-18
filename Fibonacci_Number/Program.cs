9a95df82bd3c6ad614f37c62eca5b9ed6da1d899
Bassam Rahat <bassamrahat.ibs@gmail.com>
2023-01-18 10:41:49 +0500

    first commit

---
+++ Fibonacci_Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            printSeries(number);
+            Console.ReadLine();
+        }
+
+        public static void printSeries(int number)
+        {
+            int firstNumber = 0, secondNumber = 1, nextNumber;
+
+            if(number < 2)
+            {
+                Console.WriteLine("Please Enter number greater than 2");
+            }
+            else
+            {
+                Console.Write(firstNumber + " " + secondNumber);
+                for (int i = 2; i < number; i++)
+                {
+                    nextNumber = firstNumber + secondNumber;
+                    Console.Write(" " + nextNumber);
+                    firstNumber = secondNumber;
+                    secondNumber = nextNumber;
+                }
+            }
+        }
+    }
+}
